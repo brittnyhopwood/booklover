411a914a041f46144fdeca2061ec3a8d2eaf47b6
brittnyhopwood <ymb7fb@virginia.edu>
2023-04-02 21:28:53 -0400

    add setup

---
+++ lessons/M09/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='Booklover Package',
+      url='https://github.com/brittnyhopwood/booklover'
+      author='Brittny Hopwood'
+      author_email='ymb7fb@virginia.edu'
+      license='MIT'
+      packages=['booklover'])
